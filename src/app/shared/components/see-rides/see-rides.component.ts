024d8905b4e646893ce38a5ba882e6daa93e63dc
mHavardSedeno <mathilde.havard---sedeno@etu.univ-nantes.fr>
2022-05-11 11:48:06 +0200

    Cleaning code

--- src/app/shared/components/see-rides/see-rides.component.ts
+++ src/app/shared/components/see-rides/see-rides.component.ts
@@ -11,37 +11,20 @@ import { RidesService } from '../../services/rides.service';
 export class SeeRidesComponent implements OnInit {
 
   rides: any = [];
-  displayedRides = this.rides;
   public form: FormGroup = this.fb.group({
     city: [""]
   });
 
-  @Input()
-  public set changeRides(selectedRides: any) {
-    this.displayedRides = selectedRides;
-  }
-
   constructor(private rs: RidesService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.rs.getRides().subscribe(data => {
-      this.rides = data;
-      console.log(data)})
+    this.rs.getRides().subscribe(data =>
+      this.rides = data)
   }
 
-
-/*   public getAllRides() {
-    this.rs.getRides().subscribe(data => {
-      this.displayedRides = data;
-      console.log(this.rides[0]);
-    });
-  } */
-
   public submit() {
-    console.log(this.form.value.city);
-    this.rs.searchRideByCity(this.form.value.city).subscribe(data => {
-      this.rides = data;
-      console.log(data)})
+    this.rs.searchRideByCity(this.form.value.city).subscribe(data =>
+      this.rides = data)
   }    
 
 }
