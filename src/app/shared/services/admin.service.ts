f7412cbb1284c941813dc220a0d8722afd4c0bbc
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2022-06-02 15:16:25 +0200

    change environement url

--- src/app/shared/services/admin.service.ts
+++ src/app/shared/services/admin.service.ts
@@ -1,31 +1,33 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs/internal/Observable";
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
+  private url = environment.apiURL;
 
   constructor(private http:HttpClient) { }
 
   public getAllUsers(): Observable<any>{
-    return this.http.get("http://localhost:8080/admin/getAllUsers");
+    return this.http.get(`${this.url}/admin/getAllUsers`);
   }
 
   public getAllRides(): Observable<any>{
-    return this.http.get("http://localhost:8080/admin/getAllRides");
+    return this.http.get(`${this.url}/admin/getAllRides`);
   }
 
   public updateIsAdmin(id: number, is_admin: Boolean): Observable<any>{
-    return this.http.patch("http://localhost:8080/admin/updateIsAdmin", {id, is_admin});
+    return this.http.patch(`${this.url}/admin/updateIsAdmin`, {id, is_admin});
   }
 
   public deleteRide(id: number):Observable<any>{
-    return this.http.delete(`http://localhost:8080/admin/deleteRide/${id}`);
+    return this.http.delete(`${this.url}/admin/deleteRide/${id}`);
   }
 
   public updateEnabled(id: number, enabled: Boolean):Observable<any>{
-    return this.http.patch("http://localhost:8080/admin/updateEnabled", {id, enabled});
+    return this.http.patch(`${this.url}/admin/updateEnabled`, {id, enabled});
   }
 }
