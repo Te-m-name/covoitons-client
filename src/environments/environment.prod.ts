5093acbb0f43e170420b398b81fd8e192e95ce8d
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2022-06-10 10:48:56 +0200

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   title: 'Prod Environment Heading',
-  apiURL: 'https://covoitons.herokuapp.com'
+  apiURL: 'https://covoitons.herokuapp.com',
+  googleKey: "AIzaSyCpsl3ndDdHrcZtzvBdGS2aAYWhdl5-B4c"
 };
